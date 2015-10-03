93bda42bae82f3b3ce4f04509278ec8ffcb72659
Rodrigo, Randika <randika.rodrigo@pearson.com>
2015-10-04 00:59:50 +0530

    Add initial angular project structure using yeoman generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myTodoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the myTodoApp
+ */
+angular.module('myTodoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myTodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myTodoApp
+ */
+angular.module('myTodoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
