6a29766cd5cdb3fea7c39a2630354d2830784faf
Rodrigo, Randika <randika.rodrigo@pearson.com>
2015-10-04 01:24:34 +0530

    Create a new template to show a todo as a text input list

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,6 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name myTodoApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the myTodoApp
- */
 angular.module('myTodoApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+      $scope.todos = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
   });
