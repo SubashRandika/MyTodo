6c94fc1c41e529a239158ea0b51cdfc083b28a8b
Rodrigo, Randika <randika.rodrigo@pearson.com>
2015-10-04 14:21:57 +0530

    Create new todos and validation on creation with alerts

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,5 +2,31 @@
 
 angular.module('myTodoApp')
   .controller('MainCtrl', function ($scope) {
-      $scope.todos = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+      $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+
+      $scope.addTodo = function(){
+        var myTodo = $scope.todo;
+
+        if (myTodo !== undefined) {
+            $scope.todos.push(myTodo);
+            $scope.todo = '';
+            $scope.isSuccess = true;
+            $scope.isfail = false;
+            autoCloseAlert('alert-success', 3000);
+        } else {
+            $scope.isSuccess = false;
+            $scope.isfail = true;
+            autoCloseAlert('alert-danger', 3000);
+        }
+
+      };
+
+      var autoCloseAlert = function(selector, delay){
+          window.setTimeout(function(){
+            $('.' + selector).fadeTo(500, 0).slideUp(500, function(){
+                $(this).remove();
+            });
+          }, delay);
+      };
+
   });
