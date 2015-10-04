3ae6d03fee65ba4a44e15eb211b8c111e0df9aa5
Rodrigo, Randika <randika.rodrigo@pearson.com>
2015-10-04 15:11:31 +0530

    Add feature of removing added todo as a individual todo

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,22 +11,31 @@ angular.module('myTodoApp')
             $scope.todos.push(myTodo);
             $scope.todo = '';
             $scope.isSuccess = true;
+            $scope.successMsg = 'Your Todo is successfully added...';
             $scope.isfail = false;
             autoCloseAlert('alert-success', 3000);
         } else {
             $scope.isSuccess = false;
             $scope.isfail = true;
+            $scope.failMsg = 'Please enter what you need to be done...';
             autoCloseAlert('alert-danger', 3000);
         }
 
       };
 
+      $scope.removeTodo = function(index){
+        $scope.todos.splice(index, 1);
+        $scope.isSuccess = true;
+        $scope.successMsg = 'Todo is successfully deleted...';
+        autoCloseAlert('alert-success', 3000);
+      };
+
       var autoCloseAlert = function(selector, delay){
-          window.setTimeout(function(){
-            $('.' + selector).fadeTo(500, 0).slideUp(500, function(){
-                $(this).remove();
-            });
-          }, delay);
+          $('.' + selector).fadeIn(1000, 'linear', function(){
+            window.setTimeout(function(){
+              $('.' + selector).fadeOut(1000, 'linear');
+            }, delay);
+          });
       };
 
   });
